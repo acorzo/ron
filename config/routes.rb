e1557f08264d4feb47259442720eb6c4fbb54bfa
acorzo <alexecorzo@gmail.com>
2016-12-28 20:20:24 +0000

    Set local time and latin format

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'invoices#index'
   resources :invoices
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
