69ad7e87808bc2a3e5509b4727d566b9d15d98a6
acorzo <alexecorzo@gmail.com>
2016-12-31 00:19:07 +0000

    import csv

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,8 @@
 class Company < ApplicationRecord
+  
+  def self.import(file)
+    CSV.foreach(file.path, headers: true, encoding:'iso-8859-1:utf-8') do |row|
+    Company.create!(row.to_hash)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :companies
   root 'invoices#index'
+ 
+  resources :companies do 
+    collection { post :import }
+  end  
+ 
   resources :invoices
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
